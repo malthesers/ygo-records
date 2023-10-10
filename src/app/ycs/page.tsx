3e57dddd5a969049257de4b49367d638197ba25f
malt9830 <malt9830@stud.kea.dk>
2023-10-10 15:56:50 +0200

    Implemented ycs logo + added path for public

--- src/app/ycs/page.tsx
+++ src/app/ycs/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+import logo from '~/images/logos/ycs.png'
+
 export default function YCS() {
   return (
     <main>
       <h1>Yu-Gi-Oh! Championship Series</h1>
+      <Image
+        src={logo}
+        alt='Yu-Gi-Oh! Championship Series logo'
+        blurDataURL="data:..."
+        placeholder="blur"
+        priority
+      />
     </main>
   )
 }
