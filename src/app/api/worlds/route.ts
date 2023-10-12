b06609569f85204e42157b8983f628da13ff7b0c
malt9830 <malt9830@stud.kea.dk>
2023-10-13 01:28:56 +0200

    Added location to test data in route.ts

--- src/app/api/worlds/route.ts
+++ src/app/api/worlds/route.ts
@@ -5,18 +5,21 @@ const worldsEvents:Array<Event> = [
     year: 2023,
     name: 'Paulie Aronson',
     deck: 'Bystial Dragon Link',
+    location: 'Japan',
     attendance: 28,
   },
   {
     year: 2019,
     name: 'Kouki Kosaka',
     deck: 'Salamangreat',
+    location: 'Germany',
     attendance: 28
   },
   {
     year: 2018,
     name: 'Chia Ching Wang',
     deck: 'Trickstar',
+    location: 'Japan',
     attendance: 28
   },
 ]
