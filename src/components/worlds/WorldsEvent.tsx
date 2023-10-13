358726461340eafd3ee229af941efc471a11c901
malt9830 <malt9830@stud.kea.dk>
2023-10-14 01:04:01 +0200

    Changed worlds event grid

--- src/components/worlds/WorldsEvent.tsx
+++ src/components/worlds/WorldsEvent.tsx
@@ -2,7 +2,7 @@ import { Event } from "@/interfaces/event";
 
 export default function WorldsEvent({ event }: { event: Event}) {
   return (
-    <div className="grid grid-cols-4">
+    <div className="grid grid-cols-[3rem_1fr_1fr_1fr]">
       <p>{event.year}</p>
       <p>{event.winner.nationality}</p>
       <p>{event.winner.name}</p>
