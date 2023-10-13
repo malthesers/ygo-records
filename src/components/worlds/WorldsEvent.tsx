863e8990045013653fc6feb9e9c0dc53994e11d0
malt9830 <malt9830@stud.kea.dk>
2023-10-14 00:46:48 +0200

    Added interface/props to WorldsEvent comp

--- src/components/worlds/WorldsEvent.tsx
+++ src/components/worlds/WorldsEvent.tsx
@@ -1,9 +1,12 @@
-export default function WorldsEvent() {
+import { Event } from "@/interfaces/event";
+
+export default function WorldsEvent({ event }: { event: Event}) {
   return (
     <div>
-      <p></p>
-      <p></p>
-      <p></p>
+      <p>{event.year}</p>
+      <p>{event.winner.nationality}</p>
+      <p>{event.winner.name}</p>
+      <p>{event.winner.deck}</p>
     </div>
   )
 }
