27d0a2a2d4d2b3ad8d97e70592cd4048f9a7ec00
malt9830 <malt9830@stud.kea.dk>
2024-03-08 18:37:07 +0100

    Removed commented Worlds code

--- src/app/worlds/page.tsx
+++ src/app/worlds/page.tsx
@@ -5,47 +5,16 @@ import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import logo from '~/images/logos/worlds.png'
 // import { Event } from "@/interfaces/event";
-import WorldsEvent from '@/components/worlds/WorldsEvent'
 
 // export const metadata: Metadata = {
 //   title: 'YGOTops | Yu-Gi-Oh! World Championship',
 //   description: 'YGOTops'
 // }
-
-async function getWorldsEvents() {
-  const res = await fetch('/api/worlds')
-  const data = await res.json()
-  return data
-}
-
 export default function Worlds() {
-  const [worlds, setWorlds] = useState<Array<Event>>()
-
-  // useEffect(() => {
-  //    fetch('http://localhost:3000/api/worlds')
-  //    .then((res) => res.json())
-  //    .then((data) => setWorlds(data))
-  // }, [])
-
   return (
     <main>
       <h1>Yu-Gi-Oh! World Championship</h1>
       <Image src={logo} alt='Yu-Gi-Oh! World Championship logo' priority className='max-w-md mx-auto' />
-      <table className='w-full text-center'>
-        <thead>
-          <tr className='bg-black'>
-            <th>Year</th>
-            <th>Nationality</th>
-            <th>Winner</th>
-            <th>Deck</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* {worlds?.map((event) => (
-            <WorldsEvent key={event.year} event={event} />
-          ))} */}
-        </tbody>
-      </table>
     </main>
   )
 }
