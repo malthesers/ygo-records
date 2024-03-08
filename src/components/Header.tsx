cae19ff45de8d4a2c3148a900b7467b84bb6b9a7
malt9830 <malt9830@stud.kea.dk>
2024-03-08 18:00:32 +0100

    Added initial WCQ page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,9 @@ export default function Header() {
       <nav className='flex flex-row justify-between px-6 py-3'>
         <Link href='/'>YGOTops</Link>
         <div className='flex gap-4'>
+          <Link href='/wcq'>WCQ</Link>
           <Link href='/ycs'>YCS</Link>
-          <span>|</span>
           <Link href='/team-ycs'>TEAM YCS</Link>
-          <span>|</span>
           <Link href='/remote-ycs'>Remote YCS</Link>
           {/* <Link href='/worlds'>Worlds</Link> */}
         </div>
