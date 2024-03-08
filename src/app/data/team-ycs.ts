b49d79ba71cff4015958d15999ce0ba13854633e
malt9830 <malt9830@stud.kea.dk>
2024-03-08 15:42:37 +0100

    Added additional team-ycs data

--- src/app/data/team-ycs.ts
+++ src/app/data/team-ycs.ts
@@ -43,6 +43,50 @@ const teamYcsTestData: IEvent[] = [
       },
     },
   },
+  {
+    id: 4,
+    name: 'TEAM YCS Las Vegas 2024',
+    type: {
+      name: 'TEAM YCS',
+      slug: 'team-ycs',
+    },
+    year: 2024,
+    date: 'February 24-25',
+    location: 'Las Vegas, NV, US',
+    attendance: 571,
+    winner: {
+      id: 6,
+      name: 'Hani Jawhari',
+      nationality: 'US',
+      deck: {
+        id: 4,
+        name: 'Fire King Snake-Eye',
+        archetype: 'Fire King',
+        engine: ['Snake-Eye'],
+      },
+    },
+    winner2: {
+      id: 7,
+      name: 'Hisam Jawhari',
+      nationality: 'US',
+      deck: {
+        id: 3,
+        name: 'Snake-Eye',
+        archetype: 'Snake-Eye',
+      },
+    },
+    winner3: {
+      id: 8,
+      name: 'Chris LeBlanc',
+      nationality: 'US',
+      deck: {
+        id: 4,
+        name: 'Fire King Snake-Eye',
+        archetype: 'Fire King',
+        engine: ['Snake-Eye'],
+      },
+    },
+  },
 ]
 
 export default teamYcsTestData
