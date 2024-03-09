196295a020692a6f811d9d0e65710e2825e4adee
malt9830 <malt9830@stud.kea.dk>
2024-03-09 19:37:07 +0100

    Updated CardBox to properly display card count

--- src/components/deck/CardBox.tsx
+++ src/components/deck/CardBox.tsx
@@ -7,11 +7,13 @@ interface CardBoxProps {
 }
 
 export default function CardBox({ title, cards }: CardBoxProps) {
+  const cardCount = cards.reduce((counter, currCard) => counter + currCard.count, 0)
+
   return (
     <div className='bg-sky-900 p-4'>
       <p className='flex flex-row gap-2 text-xl font-semibold'>
         <span>{title}</span>
-        <span>({cards.length})</span>
+        <span>({cardCount})</span>
       </p>
       <div>
         {cards.map((card) => (
