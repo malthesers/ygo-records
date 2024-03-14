d5d89e044766d947012ce8748e0d9c081f6e7815
malt9830 <malt9830@stud.kea.dk>
2024-03-14 21:14:02 +0100

    Added hieroglyphics banners

--- src/components/players/PlayerInfo.tsx
+++ src/components/players/PlayerInfo.tsx
@@ -1,6 +1,6 @@
 import { IPlayer } from '@/interfaces/player'
 import Image from 'next/image'
-import splash from '~/images/splash/remote-ycs.png'
+import splash from '~/images/splash/hieroglyphs-blue.png'
 
 interface PlayerInfoProps {
   player: IPlayer
@@ -8,11 +8,11 @@ interface PlayerInfoProps {
 
 export default function PlayerInfo({ player }: PlayerInfoProps) {
   return (
-    <section className='relative min-h-40 p-0 grid grid-cols-1 grid-rows-1 *:grid-center'>
-      <Image src={splash} alt='player page splash image' className='-z-10 size-full object-cover' priority fill />
-      <div className='size-full p-4'>
-        <div className='z-10 p-4 flex flex-col gap-2 items-center border-white border-4'>
-          <h1 className='mb-2 font-semibold'>{player?.name}</h1>
+    <section className='relative min-h-[15rem] p-0 grid grid-cols-1 grid-rows-1 *:grid-center'>
+      <Image className='-z-10 object-cover' src={splash} alt='hieroglyphs splash image' priority fill />
+      <div className='size-full bg-black/50 p-4'>
+        <div className='z-10 h-full p-4 flex flex-col gap-2 justify-center border-white border-4'>
+          <h1 className='mb-2 font-semibold'>{player.name}</h1>
         </div>
       </div>
     </section>
