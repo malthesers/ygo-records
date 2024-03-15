dde875b8f858c0d8a94de4d9a7395e5b9668b0f1
malt9830 <malt9830@stud.kea.dk>
2024-03-15 22:42:36 +0100

    Added more details to card page

--- src/app/cards/[slug]/page.tsx
+++ src/app/cards/[slug]/page.tsx
@@ -2,7 +2,10 @@
 
 import { testCard } from '@/app/data/test-card'
 import { ICard } from '@/interfaces/card'
+import CardInfo from '@/components/card/CardInfo'
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import cardart from '~/images/placeholders/card.webp'
 
 export default function CardPage({ params }: { params: { slug: number } }) {
   const [card, setCard] = useState<ICard | null>()
@@ -14,7 +17,8 @@ export default function CardPage({ params }: { params: { slug: number } }) {
   return (
     <main className='space-y-4'>
       <section className='grid md:grid-cols-[1fr_2fr]'>
-        <div>cardimage</div>
+        <Image src={cardart} alt='card art' className='shadow-2xl' />
+        {card && <CardInfo card={card} />}
       </section>
     </main>
   )
