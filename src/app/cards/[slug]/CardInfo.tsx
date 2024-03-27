76798361e2ca0aa4b804d7a81af1784f7d57225a
malt9830 <malt9830@stud.kea.dk>
2024-03-27 15:05:32 +0100

    Removed monter attributes from CardInfo page

--- src/app/cards/[slug]/CardInfo.tsx
+++ src/app/cards/[slug]/CardInfo.tsx
@@ -9,12 +9,12 @@ export default function CardInfo({ card }: CardInfoProps) {
     <div className='w-full bg-sky-900 my-4 p-4 border-y-2 border-white shadow-2xl space-y-2'>
       <h1 className='text-xl font-semibold text-left mb-0'>{card.name}</h1>
       <p className='bg-sky-950 p-2 whitespace-pre-wrap'>{card.description}</p>
-      <p>
+      {/* <p>
         {card.atk} / {card.def}
       </p>
       <p>{card.type}</p>
       <p>{card.attribute}</p>
-      <p>{card.level}</p>
+      <p>{card.level}</p> */}
     </div>
   )
 }
