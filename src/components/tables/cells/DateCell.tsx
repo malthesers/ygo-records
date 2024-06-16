e7f35e3f0bd4c72a9e175fd132acbbc68003b7a5
malt9830 <malt9830@stud.kea.dk>
2024-06-17 01:44:51 +0200

    Updated DateCell component

--- src/components/tables/cells/DateCell.tsx
+++ src/components/tables/cells/DateCell.tsx
@@ -6,5 +6,7 @@ interface DateCellProps {
 }
 
 export default function DateCell({ startDate, endDate }: DateCellProps) {
-  return <td className='hidden md:table-cell'>{formatDate(startDate, endDate)}</td>
+  const date = formatDate(startDate, endDate)
+
+  return <td className='hidden md:table-cell'>{date}</td>
 }
