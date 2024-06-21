516f135b8b429fc706ddf538ef1280cdebcacbaa
malt9830 <malt9830@stud.kea.dk>
2024-06-21 21:01:50 +0200

    Implemented all events in events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,9 +1,16 @@
-'use client'
+import TournamentTable from '@/components/tables/TournamentTable'
+import { IEvent } from '@/interfaces/event'
+import getData from '@/services/getData'
 
-export default function EventsPage() {
+export default async function EventsPage() {
+  const events = await getData<IEvent[]>('events')
+
+  console.log(events)
+
   return (
     <main>
       <h1>Events</h1>
+      {events && <TournamentTable events={events} />}
     </main>
   )
 }
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,19 +5,30 @@ import Image from 'next/image'
 export default function Footer() {
   return (
     <footer className='bg-sky-900'>
-      <div className='py-16 px-3 text-center grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-        <div className='sm:col-span-2 md:col-span-1'>
-          <Link href='/' className='no-underline'>
-            <Image src={logo} alt='YGO Records logo' width={100} className='mx-auto' priority />
-            <span className='font-bold text-2xl'>YGO RECORDS</span>
-          </Link>
+      <div className='py-16 px-3 text-center'>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          <div className='sm:col-span-2 md:col-span-1'>
+            <Link href='/' className='no-underline'>
+              <Image src={logo} alt='YGO Records logo' width={100} className='mx-auto' priority />
+              <span className='font-bold text-2xl'>YGO RECORDS</span>
+            </Link>
+          </div>
+          <div className='flex flex-col'>
+            <p className='text-xl font-semibold'>Links</p>
+            <Link href='/cards'>Card database</Link>
+            <Link href='/cards/popular'>Popular cards</Link>
+            <Link href='/decks'>Popular decks</Link>
+            <Link href='/events'>All events</Link>
+          </div>
+          <div className='flex flex-col'>
+            <p className='text-xl font-semibold'>Events</p>
+            <Link href='/events/wcq'>WCQ</Link>
+            <Link href='/events/ycs'>YCS</Link>
+            <Link href='/events/team-ycs'>TEAM YCS</Link>
+            <Link href='/events/remote-ycs'>Remote YCS</Link>
+          </div>
         </div>
-        <div>
-          <p>Column 2</p>
-        </div>
-        <div>
-          <p>Column 3</p>
-        </div>
+        <p className='mt-8'>a</p>
       </div>
     </footer>
   )
