d7dff409a5fd95c2d95011335f62d095f846f7cb
malt9830 <malt9830@stud.kea.dk>
2024-06-22 01:39:34 +0200

    Updated popular card page and component

--- src/app/cards/popular/page.tsx
+++ src/app/cards/popular/page.tsx
@@ -8,7 +8,7 @@ export default async function PopularCardsPage() {
   return (
     <main>
       <section>
-        <h1>Most popular cards by usage rate</h1>
+        <h1>Most popular cards</h1>
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4'>
           {popularCards.map((card, index) => (
             <PopularCard key={card.details.id} card={card} index={index} />
--- src/components/landing/popular-cards/PopularCards.tsx
+++ src/components/landing/popular-cards/PopularCards.tsx
@@ -10,7 +10,7 @@ export default async function PopularCards() {
     <section>
       <h2>Popular Cards</h2>
       <hr className='mb-4'></hr>
-      <div className='mb-4 grid gap-2 grid-cols-2 sm:grid-cols-3 md:grid-cols-6'>
+      <div className='mb-4 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-6'>
         {popularCards?.map((card, index) => (
           <PopularCard key={card.details.id} index={index} card={card} />
         ))}
