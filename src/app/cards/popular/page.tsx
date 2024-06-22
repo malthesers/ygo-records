77afc7792c350dbc31e6d95ddd181d70371b8974
malt9830 <malt9830@stud.kea.dk>
2024-06-22 06:01:14 +0200

    Added text banner in popular cards page

--- src/app/cards/popular/page.tsx
+++ src/app/cards/popular/page.tsx
@@ -1,3 +1,5 @@
+import TextBanner from '@/components/TextBanner'
+import splash from '~/images/splash/hieroglyphs-blue.png'
 import PopularCard from '@/components/landing/popular-cards/PopularCard'
 import { IPopularCard } from '@/interfaces/card'
 import getData from '@/services/getData'
@@ -7,8 +9,11 @@ export default async function PopularCardsPage() {
 
   return (
     <main>
-      <section>
-        <h1>Most popular cards</h1>
+      <TextBanner splash={splash} alt='hieroglyphs splash image'>
+        <h1 className='mb-2 font-semibold'>Most Popular Cards</h1>
+        <p className='text-lg text-center font-semibold'>Cards ranked by the percentage of decks they occur in.</p>
+      </TextBanner>
+      <section className='bg-sky-900'>
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4'>
           {popularCards.map((card, index) => (
             <PopularCard key={card.details.id} card={card} index={index} />
---
+++ src/components/TextBanner.tsx
@@ -0,0 +1,21 @@
+import Image, { StaticImageData } from 'next/image'
+import React from 'react'
+
+interface TextBannerProps {
+  splash: StaticImageData
+  alt: string
+  children?: React.ReactNode
+}
+
+export default function TextBanner({ splash, alt, children }: TextBannerProps) {
+  return (
+    <section className='relative min-h-[15rem] p-0 grid grid-cols-1 grid-rows-1 *:grid-center'>
+      <Image className='-z-10 object-cover' src={splash} alt={alt} priority fill />
+      <div className='size-full bg-black/50 p-4'>
+        <div className='z-10 size-full p-4 flex flex-col gap-2 items-center justify-center border-white border-4'>
+          {children}
+        </div>
+      </div>
+    </section>
+  )
+}
