f6dd59cc3b640b3b308bcf399b02dbcbebc6304b
malt9830 <malt9830@stud.kea.dk>
2024-06-22 04:43:19 +0200

    Added pendulum scale icons to scale info

--- src/app/cards/[slug]/card-info-box/CardInfoScale.tsx
+++ src/app/cards/[slug]/card-info-box/CardInfoScale.tsx
@@ -1,5 +1,8 @@
 import { IMonsterCard } from '@/interfaces/card/monster'
 import CardInfoBoxTemplate from './CardInfoBox'
+import scaleLeft from '~/images/frame-assets/scale-left.png'
+import scaleRight from '~/images/frame-assets/scale-right.png'
+import Image from 'next/image'
 
 interface CardInfoScaleProps {
   card: IMonsterCard
@@ -10,7 +13,11 @@ export default function CardInfoScale({ card }: CardInfoScaleProps) {
 
   return (
     <CardInfoBoxTemplate property='Scale'>
-      <p>{card.pendulum.scale}</p>
+      <div className='flex flex-row gap-2 items-center'>
+        <Image src={scaleLeft} alt='Pendulum Scale left icon' width={20} />
+        <p>{card.pendulum.scale}</p>
+        <Image src={scaleRight} alt='Pendulum Scale right icon' width={20} />
+      </div>
     </CardInfoBoxTemplate>
   )
 }
