51bc92ee3fbb219e9e1ecc3159e5f380b02fb720
malt9830 <malt9830@stud.kea.dk>
2024-06-28 14:21:33 +0200

    Added card count for visual decks

--- src/app/decks/lists/[id]/decklist-images/DeckImages.tsx
+++ src/app/decks/lists/[id]/decklist-images/DeckImages.tsx
@@ -9,9 +9,12 @@ interface DeckImagesProps {
 
 export default function DeckImages({ title, cards, className }: DeckImagesProps) {
   return (
-    <div className='bg-sky-900 p-4'>
-      <p className='text-2xl font-semibold mb-2'>{title}</p>
-      <div className={className}>
+    <div className='bg-sky-900'>
+      <p className='bg-sky-950 p-4 text-2xl font-semibold'>
+        <span>{title}</span>
+        <span> ({cards.length})</span>
+      </p>
+      <div className={`p-4 ${className || ''}`}>
         {cards.map((card, index) => (
           <DecklistCard key={index} card={card} />
         ))}
