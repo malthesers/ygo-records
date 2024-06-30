933146f937f779d7516ce1a1137845348f4cda37
malt9830 <malt9830@stud.kea.dk>
2024-06-30 12:35:39 +0200

    Implemented missing deck filler for event page

--- src/components/tables/cells/DeckCell.tsx
+++ src/components/tables/cells/DeckCell.tsx
@@ -8,7 +8,13 @@ interface DeckCellProps {
 }
 
 export default function DeckCell({ deck, deck2, deck3 }: DeckCellProps) {
-  return (
+  return deck.deckType.name === 'Unknown' ? (
+    <td className='*:block *:text-lg'>
+      <p>{deck.deckType.name}</p>
+      {deck2?.deckType?.name && <p>{deck2.deckType.name}</p>}
+      {deck3?.deckType?.name && <p>{deck3.deckType.name}</p>}
+    </td>
+  ) : (
     <td className='*:block'>
       <Link href={`/decks/lists/${deck._id}`}>{deck.deckType.name}</Link>
       {deck2?.deckType?.name && <Link href={`/decks/lists/${deck2._id}`}>{deck2.deckType.name}</Link>}
--- src/components/tables/cells/PlayerCell.tsx
+++ src/components/tables/cells/PlayerCell.tsx
@@ -1,4 +1,3 @@
-import { IWinner } from '@/interfaces/event'
 import { IPlayer } from '@/interfaces/player'
 import Link from 'next/link'
 
@@ -9,7 +8,13 @@ interface PlayerCellProps {
 }
 
 export default function PlayerCell({ player, player2, player3 }: PlayerCellProps) {
-  return (
+  return player.name === 'Unknown' ? (
+    <td className='*:block *:text-lg'>
+      <p>{player.name}</p>
+      {player2 && <p>{player2.name}</p>}
+      {player3 && <p>{player3.name}</p>}
+    </td>
+  ) : (
     <td className='*:block'>
       <Link href={`/players/${player.slug}`}>{player.name}</Link>
       {player2 && <Link href={`/players/${player2.slug}`}>{player2.name}</Link>}
